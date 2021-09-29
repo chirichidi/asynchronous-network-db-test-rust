3866584a7db89b68eb440983e64a18f86d7db1b7
chirichidi <tolessnoise@gmail.com>
2021-09-29 22:14:57 +0900

    [HL-5] Applied mutex lock to db to share across connections

--- examples/async-mini-redis-server.rs
+++ examples/async-mini-redis-server.rs
@@ -1,15 +1,26 @@
+use std::collections::HashMap;
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use std::sync::{Arc, Mutex};
+use bytes::Bytes;
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:6379").await.unwrap();
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
 
+        let db = db.clone();
+
+        println!("Accepted");
         tokio::spawn(async move  {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 
@@ -17,23 +28,21 @@ async fn main() {
     //TODO TL task 도 분리해서 병렬 처리하고 싶으면 tokio::spawn 여러쌍 생성후 채널로 파이프라이닝하면 될 듯
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
